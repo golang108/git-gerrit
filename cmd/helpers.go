36bb48aa1f411aa62bef1c8e5758ebb3e887696d
bright.ma <bright.ma@magesfc.com>
2023-04-22 22:17:05 +0800

    helpers.go: add func CaptureCommand

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -1,11 +1,12 @@
 /*
 Copyright © 2023 bright.ma <bright.ma@magesfc.com>
-
 */
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 
@@ -19,6 +20,23 @@ func ExecuteCommand(name string, subname string, args ...string) (string, error)
 	bytes, err := cmd.CombinedOutput()
 
 	return string(bytes), err
+}
+
+// Capture output but also show progress #3
+// https://blog.kowalczyk.info/article/wOYk/advanced-command-execution-in-go-with-osexec.html
+func CaptureCommand(name string, subname string, args ...string) (string, error) {
+	args = append([]string{subname}, args...)
+
+	cmd := exec.Command(name, args...)
+	//bytes, err := cmd.CombinedOutput()
+
+	var stdBuffer bytes.Buffer
+	cmd.Stdout = io.MultiWriter(os.Stdout, &stdBuffer)
+	cmd.Stderr = io.MultiWriter(os.Stderr, &stdBuffer)
+
+	err := cmd.Run()
+
+	return string(stdBuffer.Bytes()), err
 }
 
 func Error(cmd *cobra.Command, args []string, err error) {
